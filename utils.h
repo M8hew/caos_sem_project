4ea554398635e41c337267358fc38b759a7d1c83
Matthew Krivda <makrivda@edu.hse.ru>
2023-03-04 00:41:52 +0300

    added functionality

--- utils.h
+++ utils.h
@@ -4,6 +4,7 @@
 #include <linux/limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define TMPBUF_SIZE 1024
@@ -51,7 +52,8 @@ void paste(const char *path) {
     if (dst_path == NULL) {
         return;
     }
-    snprintf(src_path, PATH_MAX, "%s/%s", src_path, filename);
+
+    concat_path(src_path, filename);
     snprintf(dst_path, PATH_MAX, "%s/%s", path, filename);
 
     int src = open(src_path, O_RDONLY);
@@ -71,17 +73,28 @@ void paste(const char *path) {
     size_t bytes_read;
     while ((bytes_read = read(src, &buffer, TMPBUF_SIZE)) > 0) {
         char *offset = buffer;
-        do {
+        ssize_t bytes_write = write(dst, offset, bytes_read);
+        if (bytes_write > -1) {
+            offset += bytes_write;
+            bytes_read -= bytes_write;
+        }
+        while (bytes_read > 0) {
             ssize_t bytes_write = write(dst, offset, bytes_read);
             if (bytes_write > -1) {
                 offset += bytes_write;
                 bytes_read -= bytes_write;
             }
-        } while (bytes_read > 0);
+        }
     }
 
     close(src);
     close(dst);
+
+    struct stat st;
+    stat(src_path, &st);
+    chmod(dst_path, st.st_mode);
+
+    free(dst_path);
 
     if (otype == CUT) {
         unlink(src_path);
